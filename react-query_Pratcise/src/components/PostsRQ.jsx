80ad46f203a6d7bc580b83d1ee22cb228acdfeff
srujanGowda08 <srujangamer08@gmail.com>
2024-10-12 12:22:05 +0530

    Fetching the data using useQuery using react-quey

--- react-query_Pratcise/src/components/PostsRQ.jsx
+++ react-query_Pratcise/src/components/PostsRQ.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
 const PostsRQ = () => {
-  return <div className="container">PostsRQ</div>;
+  // Fetching data using React Query
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => {
+      return axios.get("http://localhost:4000/posts");
+    },
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+  console.log(data?.data);
+
+  return (
+    <div className="post-list">
+      {data?.data.map((post) => (
+        <div key={post.id} className="post-item">
+          <h3 className="post-title">{post.title}</h3>
+          <p className="post-body">{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostsRQ;
--- react-query_Pratcise/src/main.jsx
+++ react-query_Pratcise/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
