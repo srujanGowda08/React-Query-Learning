94a1365f998af87e5ced4d47aa54812681a24dfd
srujanGowda08 <srujangamer08@gmail.com>
2024-10-13 19:36:12 +0530

    Infinite Scroll next version using react-intersection-observer and react-quey isfetchingNextPage

--- react-query_Pratcise/src/components/InfiniteQueries.jsx
+++ react-query_Pratcise/src/components/InfiniteQueries.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from "react-intersection-observer";
 
 const fetchFruits = ({ pageParam }) => {
-  return axios.get(`http://localhost:4000/fruits/?_limit=4&_page=${pageParam}`);
+  return axios.get(`http://localhost:4000/fruits/?_limit=5&_page=${pageParam}`);
 };
 
 const InfiniteQueries = () => {
-  const { data, isLoading, isError, error, fetchNextPage, hasNextPage } =
-    useInfiniteQuery({
-      queryKey: ["fruits"],
-      queryFn: fetchFruits,
-      initialPageParam: 1,
-      getNextPageParam: (lastPage, allPages) => {
-        if (allPages.length < 5) {
-          // Assuming a total of 5 pages
-          return allPages.length + 1;
-        } else {
-          return undefined; // No more pages to load
-        }
-      },
-    });
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["fruits"],
+    queryFn: fetchFruits,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      if (allPages.length < 4) {
+        // Assuming a total of 5 pages
+        return allPages.length + 1;
+      } else {
+        return undefined; // No more pages to load
+      }
+    },
+  });
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -40,13 +56,14 @@ const InfiniteQueries = () => {
           );
         });
       })}
-      <button
+      <div ref={ref}>{isFetchingNextPage && "Loading...."}</div>
+      {/* <button
         onClick={fetchNextPage}
         className="btn"
         disabled={!hasNextPage} // Disable the button when there's no next page
       >
         Load More...
-      </button>
+      </button> */}
     </div>
   );
 };
